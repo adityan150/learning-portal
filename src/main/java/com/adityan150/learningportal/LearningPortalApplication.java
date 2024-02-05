4b01bf8f8d533892fc830d8b9b31ae18347a5f0b
adityan150 <adityanautiyal150@gmail.com>
2024-02-05 16:36:50 +0530

    init

---
+++ src/main/java/com/adityan150/learningportal/LearningPortalApplication.java
@@ -0,0 +1,13 @@
+package com.adityan150.learningportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPortalApplication.class, args);
+	}
+
+}
